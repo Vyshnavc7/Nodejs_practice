a2faead65cc0455b4b9d7a1525745ae00902f398
Vyshnav C <84959199+Vyshnavc7@users.noreply.github.com>
2022-01-03 16:07:48 +0530

    Update array.js

--- array.js
+++ array.js
@@ -2,3 +2,39 @@ var n=require('readline-sync')
 a=n.question("Enter the position")
 var s=["vyshnav","clevin"]
 console.log(s[a])
+
+#a)array manipulation, searching,sorting and splitting
+
+import numpy as npy
+
+print("Array Manipulation:") print("accessing using index")
+
+arr-npy.array(npy.arange(4,24)) for i in range(20):
+
+print(arr[i])
+
+print("Array Slicing:")
+
+print("All elements:",arr[0::])
+
+3
+
+print("Index from other end:",arr[::-1]) print("Alternate elemets starting from index 7:"arr[7::2])
+
+print("Reshaping array:")
+
+arri-arr.reshape(4,5)
+
+print(arr!)
+
+print("Vertical stacking:") a - npy.array([[12,4,5],[22,35,58] [45,33,21]]) b=npy.array([[1,30,55], [2,5,8],[5,3,1]])
+
+print(npy.vstack((a,b)))
+
+print("Searching in an array:") x= npy.where(a=35) print("position of x=",x)
+
+print("Sorting Array a:"npy.sort(a))
+
+print("Splitting Array a:") al.32,a3,a4=npy.split(arr.[4,10,16])
+
+print(al
